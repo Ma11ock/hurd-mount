7b44e1c1eee2a3d5d5a1aacd8795b2f6a5281963
Ryan Jeffrey <ryan@ryanmj.xyz>
2020-09-04 21:27:17 -0700

    argp support

--- libsysmount/mount.c
+++ libsysmount/mount.c
@@ -22,7 +22,6 @@
 #undef _PATH_MOUNTED
 #define _PATH_MOUNTED "/etc/mtab"
 
-
 struct mnt_opt_map
 {
     unsigned long intopt;
@@ -43,6 +42,19 @@ static const struct mnt_opt_map mnt_options_maps[] =
     { MS_NOSUID,        "nosuid" },
     { 0, NULL }
 };
+
+error_t
+parse_opt(int key, char *arg, struct argp_state *state)
+{
+    struct fstab_argp_params *params = state->input;
+    switch(key)
+    {
+    case ARGP_KEY_INIT:
+        state->child_inputs[0] = params; /* Pass down fstab_argp parser */
+        break;
+    }
+  return 0;
+}
 
 /* Gets mounted filesystem from path (can be relative) and stores into fs.
    Returns a pointer to a fs struct on success,
@@ -96,6 +108,7 @@ static error_t
 do_mount(struct fs *fs, bool remount, char *options,
          size_t options_len, const char *fstype)
 {
+    puts("Perf mount");
     error_t   err        = 0;
     char     *fsopts     = NULL;
     size_t    fsopts_len = 0;
@@ -288,43 +301,63 @@ end_domount:
 /* Mounts a filesystem */
 int
 mount(const char *source, const char *target,
-          const char *filesystemtype, unsigned long mountflags,
-          const void *data)
+      const char *filesystemtype, unsigned long mountflags,
+      const void *data)
 {
     /* Remount and firmlink are special because they are options for us and
        not the filesystem driver */
-    bool          remount    = false;
-    bool          firmlink   = false;
-    error_t       err        = 0;
-    struct fs    *fs         = NULL;
-    unsigned long flags      = 0;
+    bool                    remount     = false;
+    bool                    firmlink    = false;
+    error_t                 err         = 0;
+    struct fs               *fs         = NULL;
+    unsigned long           flags       = 0;
     /* Dynamic copies of `source', `target', and `filesystemtype'
        respectively */
-    char         *device     = NULL;
-    char         *mountpoint = NULL;
-    char         *fstype     = NULL;
+    char                    *device      = NULL;
+    char                    *mountpoint  = NULL;
+    char                    *fstype      = NULL;
     /* TODO assumes data is a string */
-    const char   *datastr    = (data) ? data : "";
+    const char              *datastr     = (data) ? data : "";
+    char                    *mnt_ops     = NULL;
+    size_t                   mnt_ops_len = 0;
+    /* For argp */
+    struct fstab_argp_params params;
+    char                   **mnt_argv    = malloc(sizeof(char*));
+    int                      mnt_argc    = 0;
 
+    if(!mnt_argv)
+    {
+        err = ENOMEM;
+        goto end_mount;
+    }
+    mnt_argv[0] = strdup("");
+    if(!mnt_argv[0])
+    {
+        err = ENOMEM;
+        goto end_mount;
+    }
+    mnt_argc++;
     /* Separate the per-mountpoint flags */
     if(mountflags & MS_BIND)
         firmlink = true;
     if(mountflags & MS_REMOUNT)
         remount = true;
-    if (mountflags & MS_NOSUID)
+    if(mountflags & MS_NOSUID)
         flags |= MS_NOSUID;
-    if (mountflags & MS_NODEV)
+    if(mountflags & MS_NODEV)
         flags |= MS_NODEV;
-    if (mountflags & MS_NOEXEC)
+    if(mountflags & MS_NOEXEC)
         flags |= MS_NOEXEC;
-    if (mountflags & MS_NOATIME)
+    if(mountflags & MS_NOATIME)
         flags |= MS_NOATIME;
-    if (mountflags & MS_NODIRATIME)
+    if(mountflags & MS_NODIRATIME)
         flags |= MS_NODIRATIME;
-    if (mountflags & MS_STRICTATIME)
+    if(mountflags & MS_STRICTATIME)
         flags &= ~(MS_RELATIME | MS_NOATIME);
-    if (mountflags & MS_RDONLY)
+    if(mountflags & MS_RDONLY)
         flags |= MS_RDONLY;
+    if(mountflags & MS_RELATIME)
+        flags |= MS_RELATIME;
 
     /* Check for mount options in data */
     if(strstr(datastr, "remount"))
@@ -399,26 +432,154 @@ mount(const char *source, const char *target,
         }
     }
 
+    {
+        char         *data_ops     = NULL;
+        size_t        data_ops_len = 0;
+        char         *argv_ops     = NULL;
+        size_t        argv_ops_len = 0;
+
+        inline error_t add_arg_toargv(const char *str)
+        {
+            mnt_argc++;
+            char **check = realloc(mnt_argv, mnt_argc * sizeof(char*));
+            if(!check)
+            {
+                mnt_argc--;
+                return ENOMEM;
+            }
+            mnt_argv = check;
+            mnt_argv[mnt_argc - 1] = strdup(str);
+            if(!mnt_argv[mnt_argc - 1])
+            {
+                mnt_argc--;
+                return ENOMEM;
+            }
+            return 0;
+        }
+
+        inline error_t add_op_to_str(const char *str)
+        {
+            size_t str_size = strlen(str);
+            if(!argv_ops)
+            {
+                argv_ops = malloc(str_size);
+                if(!argv_ops)
+                    return ENOMEM;
+            }
+            else
+            {
+                char  *check = realloc(argv_ops, argv_ops_len * sizeof(char)
+                                       + sizeof(char));
+                if(!check)
+                    return ENOMEM;
+                argv_ops = check;
+                argv_ops[argv_ops_len++] = ',';
+            }
+            memcpy(argv_ops + argv_ops_len, str, str_size + 1);
+            argv_ops_len += str_size;
+            return 0;
+        }
+
+        if(device)
+        {
+            err = add_arg_toargv(device);
+            if(err)
+                goto end_mount;
+        }
+        err = add_arg_toargv(mountpoint);
+        if(err)
+            goto end_mount;
+        err = add_arg_toargv("-o");
+        if(err)
+            goto end_mount;
+
+#define ARGZ(call)              \
+    err = argz_##call;          \
+    if(err)                     \
+        goto end_mount;
+
+        add_op_to_str(data);
+
+        /* TODO this assumes that data is a string, which might not be
+           correct */
+        ARGZ(create_sep(datastr, ',', &data_ops, &data_ops_len));
+
+        {
+            /* Remove `bind', `noauto', and `remount' options */
+            /* Create mnt_argv array for argp                 */
+            for(char *curstr = data_ops; curstr;
+                curstr = argz_next(data_ops, data_ops_len, curstr))
+            {
+                if((strcmp(curstr, MNTOPT_NOAUTO) != 0)
+                   && (strcmp(curstr, "bind") != 0)
+                   && (strcmp(curstr, "remount") != 0))
+                {
+                    ARGZ(add(&mnt_ops, &mnt_ops_len, curstr));
+                }
+            }
+
+        }
+        /* Add OR'd flags to option string */
+        for(size_t i = 0;
+            mnt_options_maps[i].stropt != NULL;
+            i++)
+        {
+            if(flags & mnt_options_maps[i].intopt)
+            {
+                ARGZ(add(&mnt_ops, &mnt_ops_len, mnt_options_maps[i].stropt));
+                add_op_to_str(mnt_options_maps[i].stropt);
+            }
+        }
+
+        add_arg_toargv(argv_ops);
+
+#undef ARGZ
+    }
+
+    /* Normal arguments and argp */
+    {
+        static const struct argp_option argp_opts[] =
+        {
+            {"timeout",   'T',    "MILLISECONDS", 0, "Timeout for translator startup"},
+            {"format",    'p',    "mount|fstab|translator", OPTION_ARG_OPTIONAL,
+             "Output format for query (no filesystem arguments)"},
+            {"options", 'o', "OPTIONS", 0, "A `,' separated list of options"},
+            {"readonly", 'r', 0, 0, "Never write to disk or allow opens for writing"},
+            {"writable", 'w', 0, 0, "Use normal read/write behavior"},
+            {"update", 'u', 0, 0, "Flush any meta-data cached in core"},
+            {"remount", 0, 0, OPTION_ALIAS},
+            {"verbose", 'v', 0, 0, "Give more detailed information"},
+            {"no-mtab", 'n', 0, 0, "Do not update /etc/mtab"},
+            {"test-opts", 'O', "OPTIONS", 0,
+             "Only mount fstab entries matching the given set of options"},
+            {"bind", 'B', 0, 0, "Bind mount, firmlink"},
+            {"firmlink", 0, 0, OPTION_ALIAS},
+            {"fake", 'f', 0, 0, "Do not actually mount, just pretend"},
+            {0, 0}
+        };
+
+        static const char doc[] = "Start active filesystem translators";
+        static const char args_doc[] = "\
+        DEVICE\t(in " _PATH_MNTTAB ")\n\
+        DIRECTORY\t(in " _PATH_MNTTAB ")\n\
+        [-t TYPE] DEVICE DIRECTORY\n\
+        -a";
+
+        static const struct argp_child argp_kids[] =
+            { { &fstab_argp, 0,
+                "Filesystem selection (if no explicit filesystem arguments given):", 2 },
+              { 0 } };
+
+        struct argp argp = { argp_opts, parse_opt, args_doc, doc, argp_kids };
+        argp_parse(&argp, mnt_argc, mnt_argv, 0, 0, &params);
+    }
+
     if(device) /* two-argument form */
     {
         struct fstab *fstab = NULL;
 
-        struct fstab_argp_params fstab_params =
-        {
-            fstab_path: device,
-            program_search_fmts: NULL,
-            program_search_fmts_len: 0,
-            do_all: 0,
-            types: NULL,
-            types_len: 0,
-            exclude: NULL,
-            exclude_len: 0,
-            names: NULL,
-            names_len: 0
-        };
-
         /* TODO This runs slow! */
-        fstab = fstab_argp_create(&fstab_params, SEARCH_FMTS,
+        fstab = fstab_argp_create(&params, SEARCH_FMTS,
                                   sizeof(SEARCH_FMTS));
         if(!fstab)
         {
@@ -459,54 +620,16 @@ mount(const char *source, const char *target,
         }
     }
 
-    {
-        char         *data_ops     = NULL;
-        size_t        data_ops_len = 0;
-        char         *mnt_ops      = NULL;
-        size_t        mnt_ops_len  = 0;
+    if(fs)
+        err = do_mount(fs, remount, mnt_ops, mnt_ops_len, fstype);
 
-#define ARGZ(call)              \
-    err = argz_##call;          \
-    if(err)                     \
-        goto end_mount;
-
-        /* TODO this assumes that data is a string, which might not be
-           correct */
-        ARGZ(create_sep(datastr, ',', &data_ops, &data_ops_len));
-
-        {
-            /* Remove `bind', `noauto', and `remount' options */
-            for(char *curstr = data_ops; curstr;
-                curstr = argz_next(data_ops, data_ops_len, curstr))
-            {
-                if((strcmp(curstr, MNTOPT_NOAUTO) != 0)
-                   && (strcmp(curstr, "bind") != 0)
-                   && (strcmp(curstr, "remount") != 0))
-                {
-                    ARGZ(add(&mnt_ops, &mnt_ops_len, curstr));
-                }
-            }
-
-        }
-        /* Add OR'd flags to option string */
-        for(size_t i = 0;
-            mnt_options_maps[i].stropt != NULL;
-            i++)
-        {
-            if(flags & mnt_options_maps[i].intopt)
-            {
-                ARGZ(add(&mnt_ops, &mnt_ops_len, mnt_options_maps[i].stropt));
-            }
-        }
-
-#undef ARGZ
-        if(fs)
-            err = do_mount(fs, remount, mnt_ops, mnt_ops_len, fstype);
-
-        if(data_ops)
-            free(data_ops);
-    }
 end_mount:
+    for(int i = 0; i < mnt_argc; i++)
+    {
+        free(mnt_argv[i]);
+    }
+    if(mnt_argv)
+        free(mnt_argv);
     if(device)
         free(device);
     if(mountpoint)
